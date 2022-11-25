9680cb4de859e76500be229281f3b6713f65cd22
emersonhenriquedev <emerson.hm@outlook.com>
2022-11-25 15:08:52 -0300

    estrutura inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from "../components/template/Layout"
 
 export default function Home() {
   return (
-    <div className='bg-gradient-to-r from-green-500 to-blue-500 h-screen flex justify-center items-center'>
-      <h1 className='text-3xl font-bold'>Hello world!</h1>
-    </div>
+    <Layout titulo="Página Inicial" subtitulo="construindo um template admin">
+      <h3>Conteúdo</h3>
+    </Layout>
   )
 }
